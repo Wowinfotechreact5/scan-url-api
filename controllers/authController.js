853676e792074772ff39ef6c01ef1dc5a75d3b64
Sandeepmauryaworld <wowinfo.reactdeveloper@gmail.com>
2026-03-16 11:56:02 +0530

    change pass status code change

--- controllers/authController.js
+++ controllers/authController.js
@@ -162,23 +162,24 @@ exports.changePassword = async (req, res) => {
                 });
             }
 
-            const user = result[0][0];
+           const user = result[0][0];
 
-            if (user.status === 0) {
-                return res.status(404).json({
-                    success: false,
-                    message: "User not found"
-                });
-            }
+if (!user || user.status === 0) {
+    return res.status(404).json({
+        success: false,
+        message: "User not found"
+    });
+}
 
             const match = await bcrypt.compare(currentPassword, user.password);
 
+            
             if (!match) {
-                return res.status(401).json({
-                    success: false,
-                    message: "Current password incorrect"
-                });
-            }
+    return res.status(400).json({
+        success: false,
+        message: "Current password incorrect"
+    });
+}
 
             const hashedPassword = await bcrypt.hash(newPassword, 10);
 
